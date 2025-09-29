b617ae42e8c08878e6515354cfaa5e68bd169644
Aluno01 <lucas20katolik@gmail.com>
2025-09-29 17:24:20 -0300

    criação de tela para editar as informações do restaurante

---
+++ lib/edit_restaurante.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class EditRestaurante extends StatelessWidget{
+EditRestaurante({super.key});
+
+@override
+  Widget build (BuildContext){
+  return Scaffold(
+    appBar: AppBar(title: const Text ("Editar Restaurante")),
+    body: Padding(padding: const EdgeInsets.all(30),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(),
+          Text("Edite aqui as informações do seu restaurante: "),
+SizedBox(height: 40),
+          Text("Código do restaurante"),
+DropdownButtonFormField(
+    items:[
+      DropdownMenuItem(value: "cd",child: Text("cd")),
+
+    ],
+ onChanged: (value){}),
+          SizedBox(height:25),
+          TextFormField(
+            decoration: const InputDecoration(hintText: 'Nome do Restaurante'),
+          ),
+          SizedBox(height:25),
+          TextFormField(
+            decoration: const InputDecoration(hintText: 'Nome do Restaurante'),
+          ),
+          SizedBox(height:25),
+          TextFormField(
+            decoration: const InputDecoration(hintText: 'Longitude'),
+          ),
+          SizedBox(height:25),
+          TextFormField(
+            decoration: const InputDecoration(hintText: 'Latitude'),
+          ),
+        ],
+
+      ),
+    )
+  );
+}
+}
